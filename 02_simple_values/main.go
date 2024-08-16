d49711a0ca17c468b9702df6e766b4b537fe79f5
Sahil <sahilsharma2000dtc@gmail.com>
2024-08-16 22:09:21 +0530

    simple values and noraml DS

---
+++ 02_simple_values/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple values
+	//int values
+	fmt.Println(1)
+	fmt.Println(1 + 1)
+
+	//string
+	fmt.Println("hello go lang")
+
+	//booleans
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//floats
+	fmt.Println(11.9)
+}
