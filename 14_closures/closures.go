226a128ed08e4241a5c9b0be8ae065afefd3db6d
Sahil <sahilsharma2000dtc@gmail.com>
2024-08-20 15:45:40 +0530

    closures in go lang it is same as in javascript

---
+++ 14_closures/closures.go
@@ -0,0 +1,20 @@
+//closure in go lang
+
+package main
+
+import "fmt"
+
+func counter() func() int {
+	count := 0
+
+	return func() int {
+		count++
+		return count
+	}
+}
+
+func main() {
+	increment := counter()
+	fmt.Println(increment())
+	fmt.Println(increment())
+}
