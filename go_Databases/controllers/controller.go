7da3d0c15240d3b156f78ff1021a123fe024a7f5
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-12 21:08:19 +0530

    insertOneMovie in database

--- go_Databases/controllers/controller.go
+++ go_Databases/controllers/controller.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/SAHIL-Sharma21/mongoAPI/models"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const connectionString = "mongodb+srv://Sahil:Sahil1234@cluster0.5ui1y4n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const dbName = "netflix"
 const colName = "watchlist"
 
-// IMP
 var collection *mongo.Collection
 
 // connect with mongoDB
@@ -21,6 +21,7 @@ var collection *mongo.Collection
 // it will only run at one time!!!
 func init() {
 	//client options
+	// clientoption := options.Client().ApplyURI(connectionString)
 	clientoption := options.Client().ApplyURI(connectionString)
 
 	//conect to mongo fb
@@ -36,4 +37,17 @@ func init() {
 
 	//collection instance is ready
 	fmt.Println("Collection instance is ready!")
+}
+
+//MongoDB Helpers -> they are in seperate file
+
+// inset 1
+func insertOneMovie(movie models.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("whole inserted data", inserted)
+	fmt.Println("Inserted one movie in DB with Id", inserted.InsertedID)
 }
