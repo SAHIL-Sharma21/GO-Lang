da23c71349e927962ce7e74d127c0c8c318727e4
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-14 21:01:10 +0530

    added router and tested all the apis and app is performing well

--- go_Databases/main.go
+++ go_Databases/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/SAHIL-Sharma21/mongoAPI/routers"
+)
 
 func main() {
 	fmt.Println("MongoDB API")
+
+	r := routers.Router()
+
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Server is listening at port 4000.....")
 }
